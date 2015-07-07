38b652c749e400b14252d5b9836f61d47e22bfc6
argjv <valdeiglesiasj@gmail.com>
2015-07-07 00:40:35 -0700

    Added main template example to continue working with it

---
+++ gLab/WebContent/resources/js/controllers.js
@@ -0,0 +1,11 @@
+function UsersCtrl($scope) {
+	$scope.users = [ {
+		firstname : "Jonh",
+		lastname : "Doe",
+		email : "john@example.com"
+	}, {
+		firstname : "Mary",
+		lastname : "Moe",
+		email : "mary@example.com"
+	} ];
+}
